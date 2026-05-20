549c887bae21dccb642f8727c594a114a827ea46
Fidelk2 <fidelkivumbi@gmail.com>
2026-05-20 17:09:27 +0300

    add mpesa test route

--- event-registration-laravel/routes/web.php
+++ event-registration-laravel/routes/web.php
@@ -7,3 +7,20 @@ Route::get('/', [RegistrationController::class, 'index']);
 Route::post('/register', [RegistrationController::class, 'store'])->name('register');
 Route::post('/mpesa/callback', [RegistrationController::class, 'callback'])->name('mpesa.callback');
 Route::get('/success', [RegistrationController::class, 'success'])->name('success');
+
+Route::get('/test-mpesa', function () {
+    $consumerKey    = env('MPESA_CONSUMER_KEY');
+    $consumerSecret = env('MPESA_CONSUMER_SECRET');
+
+    try {
+        $tokenResponse = \Illuminate\Support\Facades\Http::withBasicAuth($consumerKey, $consumerSecret)
+            ->get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+
+        return response()->json([
+            'status' => $tokenResponse->status(),
+            'body'   => $tokenResponse->json(),
+        ]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
